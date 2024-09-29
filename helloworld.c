151023ae8e042367eedb4aecbaca214df64366b2
yashwanthiboni <yashwanthiboni06@gmail.com>
2024-09-29 20:08:05 +0530

    CProgramming

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // writing print statement to print hello world
+    printf("Hello World");
+
+    return 0;
+}
